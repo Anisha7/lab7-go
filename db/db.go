23c5d1b904d259fa429fe8e83e5fde7a542d08a5
Anisha Jain <anishajain@outlook.com>
2019-11-13 15:23:23 -0800

    undo

--- db/db.go
+++ db/db.go
@@ -39,22 +39,22 @@ func DropTestDB() error {
 }
 
 //TODO: err check
-// func AutoMigrate(db *gorm.DB) {
-// 	db.AutoMigrate(
-// 		&model.User{},
-// 		&model.Follow{},
-// 		&model.Article{},
-// 		&model.Comment{},
-// 		&model.Tag{},
-// 	)
-// }
-
 func AutoMigrate(db *gorm.DB) {
 	db.AutoMigrate(
-		&model.Trainer{},
-		&model.Badge{},
-		&model.Pokemon{},
-		&model.Power{},
+		&model.User{},
+		&model.Follow{},
+		&model.Article{},
+		&model.Comment{},
 		&model.Tag{},
 	)
 }
+
+// func AutoMigrate(db *gorm.DB) {
+// 	db.AutoMigrate(
+// 		&model.Trainer{},
+// 		&model.Badge{},
+// 		&model.Pokemon{},
+// 		&model.Power{},
+// 		&model.Tag{},
+// 	)
+// }
